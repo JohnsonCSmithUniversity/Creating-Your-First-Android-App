78dd99054518d3706a1ec3760c3082d660ae8c8f
mafpar <mfparraperez.2015@mymail.jcsu.edu>
2019-03-29 22:53:53 -0400

    Updated for session 1

--- app/src/main/java/com/example/helloworldapp/MainActivity.java
+++ app/src/main/java/com/example/helloworldapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,19 +20,17 @@ public class MainActivity extends AppCompatActivity {
 
         //Create a Button Object that will reference a View in XML
         Button buttonOne = findViewById(R.id.button_one);
-        //Create a TextView Object that will reference a View in XML
-        final TextView textViewOne = findViewById(R.id.textView);
 
-        //set the text to '0'
-        textViewOne.setText(""+0);
-
+        //Create EditText Objects that will reference the Views in XML
+        EditText emailView = findViewById(R.id.editText);
+        EditText passwordView = findViewById(R.id.editText2);
 
         //Attach an OnClickListener to check if the button has been clicked
         buttonOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                counter++;
-                textViewOne.setText(""+counter);
+                //Display Logging in when the user click the button
+                Toast.makeText(MainActivity.this, "Loggging in", Toast.LENGTH_SHORT).show();
             }
         });
 
